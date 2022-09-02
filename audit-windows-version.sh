c105e9fecaad65dc8573598ff6708b8df95c050d
flyguy62n <rjbartels@outlook.com>
2022-09-02 17:34:16 -0400

    audit-windows-version 0.1.1

--- audit-windows-version.sh
+++ audit-windows-version.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+#   0.1.0 - Iniital release
+#   0.1.1 - Added ReleaseID and <blank> checking to keep the tables pretty(er, I hope)
+
+VERSION="0.1.1"
 
 MAX_WIDTH=$(file-width.sh)
-FORMAT_STRING="%-${MAX_WIDTH}s%-40s%-15s%-10s\n"
+FORMAT_STRING="%-${MAX_WIDTH}s%-40s%-11s%-15s%-10s\n"
 
 #Print the header row
-printf "$FORMAT_STRING" "System Name" "Product Name" "Current Build" "UBR Code"
+printf "$FORMAT_STRING" "System Name" "Product Name" "ReleaseID" "Current Build" "UBR Code"
 
 for FILE in $(ls -1 *.txt); do
     SYSTEM_NAME=${FILE/'.txt'/''}
@@ -13,12 +17,25 @@ for FILE in $(ls -1 *.txt); do
     IFS='%%%'
 
     # Get the necessary info from each file
-    PRODUCT_NAME=$(grep -m 1 '^System_OSInfo::ProductName' $FILE | awk -F ':' '{sub(/^ /,"",$4); print $4}')
-    CURRENT_BUILD=$(grep -m 1 '^System_OSInfo::CurrentBuild' $FILE | awk '{print $NF}')
-    UBR_CODE=$(grep -m 1 '^System_OSInfo::UBR' $FILE | awk '{print $NF}')
+    PRODUCT_NAME=$(grep -i -m 1 '^System_OSInfo::ProductName' $FILE | awk -F ':' '{sub(/^ /,"",$4); print $4}')
+    RELEASE_ID=$(grep -i -m 1 '^System_OSInfo::ReleaseID' $FILE | awk '{print $NF}')
+    if [[ "${RELEASE_ID}" == ":" ]] || [[ "${RELEASE_ID}" == "" ]]; then
+        RELEASE_ID="<Blank>"
+    fi
+    
+    CURRENT_BUILD=$(grep -i -m 1 '^System_OSInfo::CurrentBuild' $FILE | awk '{print $NF}')
+    if [[ "${CURRENT_BUILD}" == ":" ]] || [[ "${CURRENT_BUILD}" == "" ]]; then
+        CURRENT_BUILD="<Blank>"
+    fi
+
+    UBR_CODE=$(grep -i -m 1 '^System_OSInfo::UBR' $FILE | awk '{print $NF}')
+    if [[ "${UBR_CODE}" == ":" ]] || [[ "${UBR_CODE}" == "" ]]; then
+        UBR_CODE="<Blank>"
+    fi
+
 
     #Print the results
-    printf "$FORMAT_STRING" $SYSTEM_NAME $PRODUCT_NAME $CURRENT_BUILD $UBR_CODE
+    printf "$FORMAT_STRING" $SYSTEM_NAME $PRODUCT_NAME $RELEASE_ID $CURRENT_BUILD $UBR_CODE
     IFS=$OLD_IFS
 done
 
