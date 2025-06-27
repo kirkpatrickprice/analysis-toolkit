0573ce1b4fc538af4cf435ffda3f664b6afe00e2
Randy Bartels <rjbartels@outlook.com>
2025-06-27 15:52:37 -0400

    Replace chardet with charset-normalizer
    Signed-off-by: Randy Bartels <rjbartels@outlook.com>

--- src/kp_analysis_toolkit/utils/get_file_encoding.py
+++ src/kp_analysis_toolkit/utils/get_file_encoding.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 
-from chardet import UniversalDetector  # To detect file encoding
+from charset_normalizer import CharsetMatch, from_path
 
 
 def detect_encoding(file_path: str | Path) -> str:
     """
     Attempt to detect the encoding of the file.
 
-    Uses chardet's UniversalDetector to analyze the file content.
+    Uses charset-normalizer to analyze the file content.
 
     Args:
         file_path (str | Path): The path to the file whose encoding is to be detected.
@@ -16,13 +16,9 @@ def detect_encoding(file_path: str | Path) -> str:
         str: The detected encoding of the file, or "unknown" if detection fails.
 
     """
-    detector = UniversalDetector()
-    with Path(file_path).open("rb") as f:
-        for line in f:
-            detector.feed(line)
-            if detector.done:
-                break
-        detector.close()
-    encoding: str = detector.result.get("encoding", "unknown")
+    result: CharsetMatch | None = from_path(file_path).best()
 
-    return encoding
+    if result is not None:
+        return result.encoding
+
+    return "unknown"
