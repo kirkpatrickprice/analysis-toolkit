2ef31a3b87b46e37ead41f975fb574edecf75826
Randy Bartels <62814160+flyguy62n@users.noreply.github.com>
2025-07-09 22:07:03 -0400

    Basic definition of the Excel Export Service
    Signed-off-by: Randy Bartels <62814160+flyguy62n@users.noreply.github.com>

---
+++ src/kp_analysis_toolkit/core/services/excel_export/__init__.py
@@ -0,0 +1,6 @@
+"""Excel export service for creating Excel files throughout the toolkit."""
+
+# Add imports for the external interfaces to the service
+
+
+# add __all__: list[str] = ... to define the public API of the module for "from ... import *"
